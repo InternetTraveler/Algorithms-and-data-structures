92cb1710940e49fca1fbaa563bbe59ea39d9b64b
User Name <122952539+getSemyon@users.noreply.github.com>
2025-02-19 18:59:23 +0700

    comit lesson1

---
+++ lesson1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = sumOfEvenDegitOfNumbers(1, 100);
+            Console.WriteLine(res);
+        }
+
+        static int sumOfEvenDegitOfNumbers(int left, int right)
+        {
+            int tmp, sum = 0;
+
+            for (int number = left; number <= right; number++)
+            {
+                tmp = number;
+
+                while(tmp > 0)
+                {
+                    if (tmp % 2 == 0)
+                        sum += tmp % 10;
+
+                    tmp /= 10;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
